cea86c9303c9abfc7217dfcbb651005ca7dfc4dc
Govind Kumar <govind.kumar@i.suprdaily.com>
2021-06-13 16:22:51 +0530

    component decorator implemented

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import {Component} from "@angular/core"
+
+
+@Component({
+    selector: "my-app",
+    template: `<h1>Hello Angular</h2>`,
+    styles:[`
+    h1{
+        background-color: white
+    }
+    `],
+})
+export class AppComponent{
+
+}
